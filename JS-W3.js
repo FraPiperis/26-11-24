dae3be2e6817caf2f446fd55998c60965e1d5e1c
FraPiperis <FrancescaPiperis_00@outlook.it>
2024-11-28 17:01:53 +0100

    Add files via upload

---
+++ JS-W3.js
@@ -0,0 +1,109 @@
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "crazySum" che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma di quei due valori, ma se il loro valore
+  è lo stesso allora deve ritornare la loro somma moltiplicata per 3.
+*/
+
+/*
+function = crazySum parseInt (n1,n2) {
+    case (n1 === n2) {
+    return ((n1+n2)*3)
+    }
+    default (n1+n2) 
+}
+*/
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "boundary", che accetta un numero intero come parametro e 
+ ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.
+*/
+
+/*
+function = boundary parseInt(n) {
+    if ((20 < n >= 100) || (n === 400))
+    return true; 
+    return false
+}
+*/
+
+
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e 
+ la ritorna invertita (es.: EPICODE => EDOCIPE).
+*/
+
+/*function = reverseString("MARE") {
+    let separare = reverseString.split("MARE");
+    let invertire = reverseString.reverse(separare); 
+    let dare = reverseString.join(invertire);
+   return dare 
+}*/
+
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "upperFirst", che accetta una stringa come 
+ parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.
+*/
+
+
+/*let parola = N.split("")
+let parolafinale = []
+
+for (let p = 0; p < parola.length; p++) {
+    let primalettera = parola[p].charAt(0) 
+    console.log(primalettera)
+}*/
+
+
+  
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "giveMeRandom", che accetta come 
+ parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
+*/
+
+/*
+function = giveMeRandom (n) {
+return [nRandom(0,10) for (n in nRandom)]
+}
+*/
+
+
+
+
+//EXTRA:
+/* ESERCIZIO 1
+ Scrivi una funzione chiamata "area" che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+ Scrivi una funzione chiamata "crazyDiff" che calcola la differenza assoluta tra un numero fornito e 19.
+ Se il valore calcolato è più grande di 19, la funzione deve tornare tale risultato moltiplicato per 3.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
+ La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
+ SUGGERIMENTO: operatore modulo
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 5
+ Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
